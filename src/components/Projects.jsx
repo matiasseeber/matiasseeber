df8988fc669afbe41c057ea13bccf12a78409bc3
Matias Seeber <matiasseeber@gmail.com>
2022-11-22 23:12:23 -0300

    fixes in imgs urls

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,19 +3,19 @@ import { Project } from './Project';
 
 const projects = [
     {
-        image: "./src/assets/imgs/projects/SIA.png",
+        image: "./assets/imgs/projects/SIA.png",
         name: "Servicios Integrales Argentina",
         url: "http://serviciosintegralesarg.com/",
         description: "My first project as a freelance developer. Landing page for cleaning company, developed with HTML, CSS, SCSS and Javascript."
     },
     {
-        image: "./src/assets/imgs/projects/Portfolio.png",
+        image: "./assets/imgs/projects/Portfolio.png",
         name: "Personal portfolio",
         url: "#",
         description: "Developed with HTML, CSS, React JS, Email JS and Tailwind CSS."
     },
     {
-        image: "./src/assets/imgs/projects/AndroidStudio.png",
+        image: "./assets/imgs/projects/AndroidStudio.png",
         name: "Dont waste me",
         source: "https://github.com/matiasseeber/ProyectoFinalTUSI",
         description: "Android application developed in android studio using Java. Clone of applications like Uber Eats or Pedidos ya."
